f4900c6f907695e7626b4c7b7983ab632c20bdd3
Davide Capponcelli <davide.capponcelli@flixbus.com>
2024-11-16 23:05:56 +0100

    mysql docker compose + config + setting config

--- hello_world_django/hello_world_django/settings.py
+++ hello_world_django/hello_world_django/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'hello_world_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "OPTIONS": {
+            "read_default_file": "./my.cnf",
+        },
         # configuration for in-memory db
         # 'NAME': ':memory:',
     }
