479b72f3de7e45227b81f797405e75e800f99f1a
Davide Capponcelli <davide.capponcelli@flixbus.com>
2024-11-18 00:46:41 +0100

    model changed and migrated (removed the entity that exited)

--- hello_world_django/my_app/models.py
+++ hello_world_django/my_app/models.py
@@ -1,12 +1,36 @@
-from django.db import models
+import datetime
+from peewee import *
 
-# Create your models here.
+db = MySQLDatabase(
+    'go2rail',
+    charset = "utf8mb4",
+    sql_mode = "PIPES_AS_CONCAT",
+    use_unicode = True,
+    host = '127.0.0.1',
+    port = int('3304'),
+    user = 'go2rail',
+    password = 'password'
+)
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+class Foo(db.Model):
+    bar = CharField(max_length=50)
+    baz = IntegerField()
+    quux = IntegerField()
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class User(BaseModel):
+    username = CharField(unique=True)
+
+class Tweet(BaseModel):
+    user = ForeignKeyField(User, backref='tweets')
+    message = TextField()
+    created_date = DateTimeField(default=datetime.datetime.now)
+    is_published = BooleanField(default=True)
+
+
+MODELS = [Tweet, User, BaseModel]
+for model in MODELS:
+    model.bind(db, bind_refs=False, bind_backrefs=False)
