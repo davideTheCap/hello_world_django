0c3064602c96a740905b1ff783d6077fe66590d8
Davide Capponcelli <davide.capponcelli@flixbus.com>
2024-12-03 22:19:14 +0100

    started blank pewee project

--- hello_world_django/my_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("date", views.current_datetime),
-    path("travel-request-view", views.TravelRequestView)
-]
