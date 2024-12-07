24d8370f4bbfced7f361da3e1ec365b2e4c0a271
Davide Capponcelli <davide.capponcelli@flixbus.com>
2024-12-07 17:43:33 +0100

    removed unneeded shadow method

--- hello_world/my_app/person_repository.py
+++ hello_world/my_app/person_repository.py
@@ -5,6 +5,3 @@ from my_app.person import *
 class PersonRepository(GenericRepository[Person]):
     def __init__(self: [Person]):
         super().__init__()
-
-    def add(self, ite: Person) -> None:
-        super().add(ite)
