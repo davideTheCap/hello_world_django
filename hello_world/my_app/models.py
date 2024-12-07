990eda8bf49a1fb071766d62957ffeb943c10130
Davide Capponcelli <davide.capponcelli@flixbus.com>
2024-12-07 17:42:47 +0100

    working employees example

--- hello_world/my_app/models.py
+++ hello_world/my_app/models.py
@@ -1,9 +1,10 @@
 from my_app.person import *
 from my_app.person_repository import PersonRepository
 
-# from playhouse.shortcuts import model_to_dict
-
-emp = Person("Satyam", 102)  # An Object of Person
-emp.Display()
 person_repo = PersonRepository()
-person_repo.add(emp)
+person_repo.add(Person("John", 1))
+person_repo.add(Person("Bob", 2))
+
+employees = person_repo.all()
+for employee in employees:
+    print(employee.name)
